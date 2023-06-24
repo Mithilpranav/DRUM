daea3e2b2b127d8ccfbbd2435baa0d0f574f6983
Mithilpranav <121457554+Mithilpranav@users.noreply.github.com>
2023-06-24 09:30:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,32 +31,34 @@ function makesound(t){
     switch(t)
     {
      case "w":
-         var g = new Audio("sounds/tom-1.mp3");
+         var g = new Audio("DRUM/sounds/tom-1.mp3");
          g.play();
          break;
 
             
     case "a":
-         var g = new Audio("sounds/tom-2.mp3");
+         var g = new Audio("DRUM/sounds/tom-2.mp3");
          g.play();
          break;
      case "d":
-         var g = new Audio("sounds/tom-3.mp3");
+         var g = new Audio("DRUM/sounds/tom-3.mp3");
          g.play();
          break;
      case "s":
-         var g = new Audio("sounds/tom-4.mp3");
+         var g = new Audio("DRUM/sounds/tom-4.mp3");
          g.play();
          break;
      case "j":
-         var g = new Audio("sounds/tom-5.mp3");
+         var g = new Audio("DRUM/sounds/tom-5.mp3");
          g.play();
          break;
         case "k":
-            var g = new Audio("sounds/tom-6.mp3");
+            var g = new Audio("DRUM/sounds/tom-6.mp3");
+            g.play();
             break;
         case "j":
-            var g = new Audio("sounds/tom-7.mp3");
+            var g = new Audio("DRUM/sounds/tom-7.mp3");
+            g.play();
             break;
     
     
